31cc5a9cd6c48285152729feb1d55a9776713e03
shengyong.heng <3271660881@qq.com>
2025-07-01 13:26:48 +0800

    tasklist component

---
+++ components/TaskList.jsx
@@ -0,0 +1,38 @@
+import { FlatList, Text, View } from "react-native";
+import { styles } from "./styles";
+import { Task } from "./Task";
+
+export const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
+  const events = {
+    onPinTask,
+    onArchiveTask,
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.listItems}>
+        <Text>loading</Text>
+      </View>
+    );
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <View style={styles.listItems}>
+        <Text>empty</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.listItems}>
+      <FlatList
+        data={tasks}
+        keyExtractor={(task) => task.id}
+        renderItem={({ item }) => (
+          <Task key={item.id} task={item} {...events} />
+        )}
+      />
+    </View>
+  );
+};
